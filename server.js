7fa04f2392b3ea55cf08a0c5d9a6356cad79b273
Zakojurnikov <d.zakojurnikov@gmail.com>
2021-09-24 17:23:14 +0500

    fixed server.js

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
 	static: './build',
-})
-
-const PORT = process.env.PORT || 3001;
+});
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(PORT, () => {
-	console.log('Server is running');
-})
+server.listen(port);
